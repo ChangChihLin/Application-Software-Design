d7e797234d7d337b505baeaddccdff32337df237
ChangChihLin <m107025201@gmail.com>
2016-10-04 22:58:40 +0800

    Lab homework

---
+++ App Project/Lab1/app/src/main/java/m107025201/lab1/Lab1AppProject.java
@@ -0,0 +1,13 @@
+package m107025201.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab1AppProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1_app_project);
+    }
+}
